3e26573d4109ebcf7a6f346ff1f0e7aebca67ef4
MCC-Maritza <M.C.C.Maritza@outlook.es>
2023-02-23 12:12:39 -0600

    Se sube programa

---
+++ src/dialogodeinstalacion/DialogoDeInstalacion.java
@@ -0,0 +1,20 @@
+package dialogodeinstalacion;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author maryse
+ *
+ * JOptionPane Este programa ilustra el cuadro de diálogo del objeto
+ * JOptionPane.
+ *
+ */
+public class DialogoDeInstalacion {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Antes de comenzar la instalación, " + "cierre todas las aplicaciones.");
+    }
+
+}
